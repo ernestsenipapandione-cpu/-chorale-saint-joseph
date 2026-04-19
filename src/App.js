a521980308780932bee65340aa50b3f75e293caa
Ernest NDIONE <ernestsenipapa.ndione@unchk.edu.sn>
2026-04-19 21:55:50 +0000

    fix: mise en page responsive de la sidebar et correction des routes du dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,3 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'
-import Layout from './components/Layout' // Chemin corrigé
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
-import Members from './pages/Members'
-import Calendar from './pages/Calendar'
-import Chat from './pages/Chat'
-import Partitions from './pages/Partitions'
-import Live from './pages/Live'
-import Contact from './pages/Contact'
-import Contacts from './pages/Contacts'
-import Finances from './pages/Finances'
-import Settings from './pages/Settings'
-import Merci from './pages/Merci'
-
-const DashboardLayout = () => (
-  <Layout>
-    <Outlet />
-  </Layout>
-);
-
 function App() {
   return (
     <BrowserRouter>
@@ -29,20 +7,17 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/merci" element={<Merci />} />
 
-        <Route element={<DashboardLayout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/members" element={<Members />} />
-          <Route path="/calendar" element={<Calendar />} />
-          <Route path="/chat" element={<Chat />} />
-          <Route path="/partitions" element={<Partitions />} />
-          <Route path="/live" element={<Live />} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/finances" element={<Finances />} />
-          <Route path="/settings" element={<Settings />} />
-        </Route>
+        {/* On enlève DashboardLayout et on met les routes normalement */}
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/members" element={<Members />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/partitions" element={<Partitions />} />
+        <Route path="/live" element={<Live />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/finances" element={<Finances />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </BrowserRouter>
   )
 }
-
-export default App
